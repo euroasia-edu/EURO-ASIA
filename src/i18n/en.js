ff2b7baca7dbe157d413152d79cc6224d2bf6043
dumitrw <nubitrwgt@gmailcom>
2026-01-10 21:02:34 +0200

    new file:   src/i18n/en.js 	new file:   src/i18n/ro.js 	modified:   src/pages/en/index.astro 	modified:   src/pages/index.astro 	modified:   src/pages/ro/index.astro

---
+++ src/i18n/en.js
@@ -0,0 +1,8 @@
+export default {
+  home: {
+    title: "ABOUT US",
+    text: `We are passionate about international education and experiences that broaden horizons. We facilitate cultural exchanges, study programs, and complete student support, ensuring safe, comfortable, and memorable mobility experiences.`,
+    text2: `Our goal is for every student to leave with new knowledge, international friendships, and memories that inspire. We work alongside partners in Romania, Italy, and Turkey to make each mobility a real and opportunity-filled educational experience.`,
+    image: "https://i.imgur.com/Ub3auHi.jpeg",
+  }
+};
---
+++ src/i18n/ro.js
@@ -0,0 +1,8 @@
+export default {
+  home: {
+    title: "DESPRE NOI",
+    text: `Suntem pasionați de educația internațională și de experiențele care deschid orizonturi. Facilităm schimburi culturale, programe de studiu și suport complet pentru studenți, asigurând mobilități sigure, confortabile și memorabile.`,
+    text2: `Scopul nostru este ca fiecare student să plece acasă cu noi cunoștințe, prietenii internaționale și amintiri care inspiră. Lucrăm alături de parteneri din România, Italia și Turcia pentru a transforma fiecare mobilitate într-o experiență educațională reală și plină de oportunități.`,
+    image: "https://i.imgur.com/Ub3auHi.jpeg"
+  }
+};
