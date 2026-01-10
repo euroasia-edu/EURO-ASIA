6581d3ffdcc66f7454c1042a13a106ccbcdd5f3b
dumitrw <nubitrwgt@gmailcom>
2026-01-10 21:20:57 +0200

    modified:   src/middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,20 @@
-import { defineMiddleware } from "astro/middleware";
+// import { defineMiddleware } from "astro/middleware";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  const { request, cookies, url } = context;
+// export const onRequest = defineMiddleware(async (context, next) => {
+//   const { request, cookies, url } = context;
 
-  // rulează DOAR pe homepage
-  if (url.pathname !== "/") {
-    return next();
-  }
+//   // rulează DOAR pe homepage
+//   if (url.pathname !== "/") {
+//     return next();
+//   }
 
-  const lang = cookies.get("lang")?.value;
+//   const lang = cookies.get("lang")?.value;
 
-  // dacă userul a ales o limbă înainte
-  if (lang === "ro" || lang === "en") {
-    return Response.redirect(new URL(`/${lang}`, url), 302);
-  }
+//   // dacă userul a ales o limbă înainte
+//   if (lang === "ro" || lang === "en") {
+//     return Response.redirect(new URL(`/${lang}`, url), 302);
+//   }
 
-  // fallback default
-  return Response.redirect(new URL("/ro", url), 302);
-});
+//   // fallback default
+//   return Response.redirect(new URL("/ro", url), 302);
+// });
