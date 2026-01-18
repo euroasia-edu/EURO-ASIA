20f2365e5f6a834b23e344278a475242865a44ec
dumitrw <nubitrwgt@gmailcom>
2026-01-18 19:29:30 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/[lang]/services.astro 	deleted:    src/scripts/lang-redirect.js

--- src/scripts/lang-redirect.js
+++
@@ -1,5 +0,0 @@
-// src/scripts/lang-redirect.js
-const lang = localStorage.getItem('lang') || navigator.language.startsWith('en') ? 'en' : 'ro';
-if (window.location.pathname === '/') {
-  window.location.replace(`/${lang}`);
-}
